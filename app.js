a3d439f0e1e1c32abdbf391d50d3870a205e9b23
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-28 20:17:34 +0300

    add structure mongoose & connect localhost

---
+++ app.js
@@ -0,0 +1,40 @@
+var bodyParser = require("body-parser"),
+express        = require("express"),
+mongoose       = require("mongoose"),
+app            = express();
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// MONGOOGSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+Blog.create({
+  title: "TestBlog"
+  image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"
+  body: "Hello its a blog post"
+});
+
+
+// RESTFUL ROUTES
+app.listen(process.env.PORT, process.env.IP, function(){
+  console.log("SERVER IS RUNNING!");
+});
+
+
+
+
+
+
+
+app.listen(3000);
